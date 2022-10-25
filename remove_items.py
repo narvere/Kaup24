47008d3ab5f644b425feb724ded44f6eb4a2bddf
Deniss Hohlov <deniss.hohlov@gmail.com>
2022-10-25 15:34:10 +0300

    ramps

--- remove_items.py
+++ remove_items.py
@@ -2,50 +2,10 @@ import csv
 import openpyxl
 
 
-def css_reader():
-    """
-    Читаю csv файл со всеми товарами с магазина и результат записывает в список arr в виде массива
-    :return: ничего
-    """
-    arr = []
-    with open('Kaup24.ee_kaup.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in spamreader:
-            # row[0] = text + row[0]
-            arr.append(row)
-    print(arr)
-
-
-def delete_row_from_css():
-    """
-    Нахождение по штртх-коду и удаление целой строки. Создание нового файла.
-    :return: ничего
-    """
-    with open('Kaup24.ee_kaup.csv', 'r') as inp, open('Kaup24.ee_kaup_edit.csv', 'w', newline='') as out:
-        writer = csv.writer(out)
-        for row in csv.reader(inp):
-            if row[4] != "6972401119183":
-                writer.writerow(row)
-
-
-def column_printout():
-    """
-    Вывод всех знчений одного столбца xlsx таблицы
-    :return: ничего
-    """
-    path = "supplierProducts.xlsx"
-    wb_obj = openpyxl.load_workbook(path)
-    sheet_obj = wb_obj.active
-    m_row = sheet_obj.max_row
-    for i in range(1, m_row + 1):
-        cell_obj = sheet_obj.cell(row=i, column=1)
-        print(str(cell_obj.value))
-
-
 def set_maker():
     """
     создает множество из штрихкодов всех товаров
-    :return: ничего
+    :return: dicts_set множество из штрихкодов всех товаров и dicts словарь со всеми товарами
     """
     dicts = {}
     arr = []
@@ -55,18 +15,42 @@ def set_maker():
             arr.append(row)
         for i in arr:
             dicts[i[4]] = [i[0], i[1], i[2], i[3]]
-    print(set(dicts.keys()))
-
-# css_reader()
-# delete_row_from_css()
-# column_printout()
-# set_maker()
+    dicts_set = set(dicts.keys())
+    return dicts_set, dicts
 
 
-path = "supplierProducts.xlsx"
-wb_obj = openpyxl.load_workbook(path)
-sheet_obj = wb_obj.active
-m_row = sheet_obj.max_row
-for i in range(1, m_row + 1):
-    cell_obj = sheet_obj.cell(row=i, column=1)
-    print(str(cell_obj.value))
+def del_set_maker(path):
+    """
+    создаеи множество из удаляемых щтрихкодов
+    :return: это множество из удаляемых щтрихкодов
+    """
+    barcodes_del = set()
+    wb_obj = openpyxl.load_workbook(path)
+    sheet_obj = wb_obj.active
+    m_row = sheet_obj.max_row
+    for i in range(1, m_row + 1):
+        cell_obj = sheet_obj.cell(row=i, column=1)
+        # print(str(cell_obj.value))
+        barcodes_del.add(str(cell_obj.value))
+    return barcodes_del
+
+
+def sell_dict_full():
+    """
+    используя два множества получаю готовый список для экспорта товара
+    :return:
+    """
+    lst = []
+    full_set, dicts = set_maker()
+    del_set = del_set_maker("supplierProducts.xlsx") | del_set_maker("existBarcodes.xlsx")
+    finish_set = full_set - del_set
+    # print(len(finish_set))
+    # print(len(dicts))
+    new_dicts = {}
+    for key, value in dicts.items():
+        if key in finish_set:
+            new_dicts[key] = value
+    print(new_dicts)
+
+
+sell_dict_full()
